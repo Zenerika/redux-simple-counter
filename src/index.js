4998ae633d0880c2ddf4d6c80149af247180319f
Aguirre <zenerika@ymail.com>
2019-02-07 16:09:52 -0600

    working on bonus

--- src/index.js
+++ src/index.js
@@ -9,4 +9,22 @@ subscribe(() => render(getState()))
 
 // Dispatch the "INCREMENT" action every time the +1 button is pressed
 const incrementButton = document.getElementById('increment');
-incrementButton.addEventListener('click', e => dispatch({ type: "INCREMENT" }));
+incrementButton.addEventListener('click', e => dispatch({ type: "MATH", value: 1 }));
+
+const decrementButton = document.getElementById('decrement');
+decrementButton.addEventListener('click', e => dispatch({ type: "MATH", value: -1 }));
+
+const incrementButtonBy5 = document.getElementById('incrementBy5');
+incrementButtonBy5.addEventListener('click', e => dispatch({ type: "MATH", value: 5 }));
+
+const decrementButtonBy5 = document.getElementById('decrementBy5');
+decrementButtonBy5.addEventListener('click', e => dispatch({ type: "MATH", value: -5 }));
+
+const changeColor = document.getElementById('changecolor');
+changeColor.addEventListener('click', e => dispatch({ type: "CHANGE_COLOR" }));
+
+const manualCounter = document.getElementById('manualcounter');
+manualCounter.addEventListener('click', e => dispatch({ type: "MANUAL_COUNTER" }));
+
+const addCounter = document.getElementById('addcounter');
+addCounter.addEventListener('click', e => dispatch({ type: "ADD_COUNTER" }));
--- src/reducer.js
+++ src/reducer.js
@@ -1,12 +1,32 @@
 const initialState = {
-    value: 0
+    value: 0,
+    color: 'red'
 }
 
 const reducer = (state = initialState, action) => {
     let { type } = action;
-    if (type === "INCREMENT") {
+    if (type === "MATH") {
         return {
-            value: state.value + 1
+            value: state.value + action.value,
+            color: state.color
+        }
+    }
+    else if (type === "CHANGE_COLOR") {
+        return {
+            value: state.value,
+            color: state.color = changeColor.value
+        }
+    }
+    else if (type === "MANUAL_COUNTER") {
+        return {
+            value: parseInt(manualcounter.value),
+            color: state.color = changeColor.value
+        }
+    }
+    else if (type === "ADD_COUNTER") {
+        return {
+            value: state.value,
+            color: state.color
         }
     }
     return state
